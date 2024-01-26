e56bdffd042be39d37074e36350b7333cacc0f08
Samantha <samanthavillafana8@gmail.com>
2024-01-25 22:07:46 -0500

    complete lab

--- main.js
+++ main.js
@@ -4,6 +4,45 @@ const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
 
+//Get a refernce to the error modal element + add 'hidden' class
+const errorModal = document.getElementById('modal')
+const errorMessage = document.getElementById('modal-message')
+errorModal.classList.add("hidden");
+
+//Grab the emptyHeart and iterate through each
+const emptyHeart = document.querySelectorAll('.like-glyph')
+emptyHeart.forEach((element) => {
+  element.addEventListener('click', function() {
+    //Stimulate making a server request 
+    mimicServerCall()
+      .then(response => {
+        console.log(response);
+
+        //Handle the successful response here
+        //Check if the heart has the .activated-heart class
+    if (element.classList.contains('activated-heart')) {
+      //Remove the .activated-heart class
+      element.classList.remove('activated-heart');
+      //Change the heart back to empty
+      element.textContent = EMPTY_HEART
+    }
+    else {
+      element.classList.add('activated-heart');
+        element.textContent = FULL_HEART
+    }
+      })
+      .catch(error => {
+        console.error(error);
+        //Handle error
+        errorMessage.textContent = 'Server request failed! Please try again.';
+        errorModal.classList.remove('hidden');
+      setTimeout(() => {
+        errorModal.classList.add('hidden');
+      }, 3000);
+      });
+      });
+
+  })
 
 
 
@@ -23,3 +62,5 @@ function mimicServerCall(url="http://mimicServer.example.com", config={}) {
     }, 300);
   });
 }
+
+
